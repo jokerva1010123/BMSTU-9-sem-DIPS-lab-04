d7ffab08e30242856681ce454f6bd866e4166354
Dinh Viet Anh <jokerva1010123@gmail.com>
2024-12-19 00:13:24 +0300

    Update

--- CarRentalSystem/ModelsDTO/Cars/CarInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModelsDTO.Cars;
-
-public class CarInfo
-{
-    public Guid CarUid { get; set; }
-    public string Brand { get; set; }
-    public string Model { get; set; }
-    public string RegistrationNumber { get; set; }
-}
